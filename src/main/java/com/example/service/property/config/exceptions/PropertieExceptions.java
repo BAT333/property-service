65dc6cba9bc6fd559b84140bf45d04c6e67a24e5
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-08-02 19:15:42 -0300

    Creating exceptions

---
+++ src/main/java/com/example/service/property/config/exceptions/PropertieExceptions.java
@@ -0,0 +1,7 @@
+package com.example.service.property.config.exceptions;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PropertieExceptions extends RuntimeException{
+}
