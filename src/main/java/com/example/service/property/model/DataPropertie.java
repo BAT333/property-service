8a68d4ae2ebff3dcbe8b40ceac754b5bb89782ba
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-10-13 14:16:01 -0300

    logging the application and refactoring the code by removing functions that are no longer used

--- src/main/java/com/example/service/property/model/DataPropertie.java
+++ src/main/java/com/example/service/property/model/DataPropertie.java
@@ -3,6 +3,7 @@ import com.example.service.property.domain.ContractType;
 import com.example.service.property.domain.Propertie;
 import com.example.service.property.domain.PropertyType;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 public record DataPropertie(
@@ -13,7 +14,7 @@ public record DataPropertie(
         ContractType type,
         PropertyType propertyType,
         DataAddressDTO address
-) {
+) implements Serializable {
     public DataPropertie(Propertie propertie) {
         this(propertie.getId(), propertie.getQuantityRoom(), propertie.getPrice(),
                 propertie.getDescription(), propertie.getType(),propertie.getPropertyType(),new DataAddressDTO(propertie.getAddress()));
